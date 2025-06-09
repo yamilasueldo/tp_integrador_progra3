f51b1fc4fbf646bd6d2c293db1c87bc17266f2d5
Tomas Blanco <tomiblanco98@gmail.com>
2025-06-09 04:04:02 -0300

    Correción de Modelo, Vista y Control en productos

---
+++ Estaticos/js/producto.js
@@ -0,0 +1,97 @@
+class Producto{
+    id;
+    nombre;
+    categoria;
+    precio;
+    imagen;
+    cantidad;
+
+    constructor(id, nombre, categoria, precio, imagen, cantidad) {
+        this.id = id;
+        this.nombre = nombre;
+        this.categoria = categoria;
+        this.precio = precio;
+        this.imagen = imagen;
+        this.cantidad = cantidad;
+    }
+
+    toJsonString() {
+        return JSON.stringify(this);
+    }
+
+    static createFromJsonString(json) {
+        const jsonParsed = JSON.parse(json);
+        return new Producto(jsonParsed.id, jsonParsed.nombre, jsonParsed.categoria, jsonParsed.precio, jsonParsed.imagen, jsonParsed.cantidad);
+    }
+
+    createHtmlElement(){
+        const div = document.createElement("div");
+        div.classList.add("card");
+
+        const estaEnCarrito = carrito.some(item => item.id === this.id);
+
+        div.innerHTML = `
+            <div class="card-image">
+                <img src="${this.imagen}" alt="">
+            </div>
+            <div class="card-info flow">
+                <h3 class="card-title">${this.nombre}</h3>
+                <ul class="tag-list" role="list">
+                    <li>${this.categoria}</li>
+                </ul>
+            </div>
+            <div class="button-container" data-align="end" style="display: ${estaEnCarrito ? 'flex' : 'none'};">
+                <button id="quitarBtn" class="button">Quitar</button>
+            </div>
+            <div class="button-container">
+                <p class="card-price">$${this.precio}</p>
+                <button id="agregarBtn" class="button">Agregar</button>
+            </div>
+        `;
+
+        const btnAgregar = div.querySelector("#agregarBtn");
+        btnAgregar.addEventListener("click", (e) => {
+            this.agregarProducto();
+            guardarYActualizar();
+            control.cargaInicial();
+        });
+
+        const btnQuitar = div.querySelector("#quitarBtn");
+        if (btnQuitar) {
+            btnQuitar.addEventListener("click", () => {
+                const productoEnCarrito = carrito.find(p => p.id === this.id);
+                
+                if (!productoEnCarrito) {
+                    alert("Este producto no está en el carrito.");
+                    return;
+                }
+
+                if (productoEnCarrito.cantidad > 1) {
+                    productoEnCarrito.cantidad--;
+                } else {
+                    carrito = carrito.filter(p => p.id !== this.id);
+                }
+
+                guardarYActualizar();
+                control.cargaInicial();
+                const nuevoElemento = this.createHtmlElement();
+                div.replaceWith(nuevoElemento);
+            });
+        }
+
+        return div;
+    }
+
+    agregarProducto(){
+        const productoAgregado = {
+            id: this.id,
+            nombre: this.nombre,
+            categoria: this.categoria,
+            precio : this.precio,
+            imagen: this.imagen,
+            cantidad: this.cantidad
+        };
+        
+        carrito.push(productoAgregado);
+    }
+}
--- Estaticos/js/productos.js
+++ Estaticos/js/productos.js
@@ -1,72 +1,168 @@
-fetch("../../Estaticos/js/productos.json")
-    .then(res => res.json())
-    .then(productos => {
-        mostrarProductos(productos);
-        window.productos = productos;
-    });
+// fetch("../../Estaticos/js/productos.json")
+//     .then(res => res.json())
+//     .then(productos => {
+//         mostrarProductos(productos);
+//         window.productos = productos;
+//     });
 
-function mostrarProductos(lista) {
-    const contenedor = document.querySelector(".section.grid-auto-fill");
-    contenedor.innerHTML = "";
+// function mostrarProductos(lista) {
+//     const contenedor = document.querySelector(".section.grid-auto-fill");
+//     contenedor.innerHTML = "";
 
-    lista.forEach(producto => {
-        const card = document.createElement("div");
-        card.classList.add("card");
-        const estaEnCarrito = carrito.some(item => item.id === producto.id);
+//     lista.forEach(producto => {
+//         const card = document.createElement("div");
+//         card.classList.add("card");
+//         const estaEnCarrito = carrito.some(item => item.id === producto.id);
 
-        card.innerHTML = `
-            <div class="card-image">
-                <img src="${producto.imagen}" alt="">
-            </div>
-            <div class="card-info flow">
-                <h3 class="card-title">${producto.nombre}</h3>
-                <ul class="tag-list" role="list">
-                    <li>${producto.categoria}</li>
-                </ul>
-            </div>
-            <div class="button-container" data-align="end" style="display: ${estaEnCarrito ? 'flex' : 'none'};">
-                <button id="btnQuitar" class="button">Quitar</button>
-            </div>
-            <div class="button-container">
-                <p class="card-price">$${producto.precio}</p>
-                <button id="btnAgregar" class="button">Agregar</button>
-            </div>
-        `;
+//         card.innerHTML = `
+//             <div class="card-image">
+//                 <img src="${producto.imagen}" alt="">
+//             </div>
+//             <div class="card-info flow">
+//                 <h3 class="card-title">${producto.nombre}</h3>
+//                 <ul class="tag-list" role="list">
+//                     <li>${producto.categoria}</li>
+//                 </ul>
+//             </div>
+//             <div class="button-container" data-align="end" style="display: ${estaEnCarrito ? 'flex' : 'none'};">
+//                 <button id="btnQuitar" class="button">Quitar</button>
+//             </div>
+//             <div class="button-container">
+//                 <p class="card-price">$${producto.precio}</p>
+//                 <button id="btnAgregar" class="button">Agregar</button>
+//             </div>
+//         `;
 
-        contenedor.appendChild(card);
+//         contenedor.appendChild(card);
 
-        const botonAgregar = card.querySelector("#btnAgregar");
-        botonAgregar.addEventListener("click", () => {
-            agregarAlCarrito(producto);
-            mostrarProductos(window.productos);
+//         const botonAgregar = card.querySelector("#btnAgregar");
+//         botonAgregar.addEventListener("click", () => {
+//             agregarAlCarrito(producto);
+//             mostrarProductos(window.productos);
+//         });
+
+//         const btnQuitar = card.querySelector("#btnQuitar");
+//         if (btnQuitar) {
+//             btnQuitar.addEventListener("click", () => {
+//                 const productoEnCarrito = carrito.find(p => p.id === producto.id && p.talle === producto.talle);
+                
+//                 if (!productoEnCarrito) {
+//                     alert("Este producto no está en el carrito.");
+//                     return;
+//                 }
+
+//                 if (productoEnCarrito.cantidad > 1) {
+//                     productoEnCarrito.cantidad--;
+//                 } else {
+//                     carrito = carrito.filter(p => p.id !== producto.id || p.talle !== producto.talle);
+//                 }
+
+//                 guardarYActualizar();
+//                 mostrarProductos(window.productos);
+//             });
+//         }
+//     });
+// }
+
+// function filterProducts(categoria) {
+//     const filtrados = categoria === "all"
+//         ? window.productos
+//         : window.productos.filter(p => p.categoria === categoria);
+//     mostrarProductos(filtrados);
+// }
+
+// ARREGLOS
+
+class Modelo {
+    Productos;
+
+    constructor() {}
+}
+
+class Vista {
+    ProductosContainer;
+
+    constructor() {
+        this.SeccionPrincipal = {
+            divProductos: this.$("productos-container"),
+        }
+    }
+
+    $(id){
+        return document.getElementById(id);
+    }
+}
+
+class Control {
+    Vista;
+    Modelo;
+
+    constructor(p_vista, p_modelo) {
+        this.Modelo = p_modelo;
+        this.Vista = p_vista;
+
+        this.cargaInicial();
+
+        document.getElementById("filtro-todos").addEventListener("click", () => {
+            this.filterProducts("all");
         });
 
-        const btnQuitar = card.querySelector("#btnQuitar");
-        if (btnQuitar) {
-            btnQuitar.addEventListener("click", () => {
-                const productoEnCarrito = carrito.find(p => p.id === producto.id && p.talle === producto.talle);
-                
-                if (!productoEnCarrito) {
-                    alert("Este producto no está en el carrito.");
-                    return;
-                }
+        document.getElementById("filtro-ropa").addEventListener("click", () => {
+            this.filterProducts("ropa");
+        });
 
-                if (productoEnCarrito.cantidad > 1) {
-                    productoEnCarrito.cantidad--;
-                } else {
-                    carrito = carrito.filter(p => p.id !== producto.id || p.talle !== producto.talle);
-                }
+        document.getElementById("filtro-accesorios").addEventListener("click", () => {
+            this.filterProducts("accesorios");
+        });
+    }
 
-                guardarYActualizar();
-                mostrarProductos(window.productos);
-            });
-        }
-    });
+    cargaInicial() {
+        this.Vista.SeccionPrincipal.divProductos.innerHTML = "";
+
+        fetch("../../Estaticos/js/productos.json")
+        .then(res => res.json())
+        .then(data => {
+            let productos = data.map(productoData => {
+                return new Producto(
+                    productoData.id,
+                    productoData.nombre,
+                    productoData.categoria,
+                    productoData.precio,
+                    productoData.imagen,
+                    productoData.cantidad
+                );
+            })
+            this.Modelo.Productos = productos;
+            this.actualizarVistaProductos(productos);
+        })
+        .catch(error => {
+            console.error("Error al cargar los productos:", error);
+        })
+        .finally(() => {
+            console.log("Promesa finalizada");
+        });
+    }
+
+    actualizarVistaProductos(lista) {
+        this.Vista.SeccionPrincipal.divProductos.innerHTML = "";
+
+        lista.forEach(producto => {
+            const htmlElement = producto.createHtmlElement();
+            this.Vista.SeccionPrincipal.divProductos.appendChild(htmlElement);
+        });
+    }
+
+    filterProducts(categoria) {
+        let productos = this.Modelo.Productos;
+
+        const filtrados = categoria === "all"
+            ? productos
+            : productos.filter(p => p.categoria === categoria);
+
+        this.actualizarVistaProductos(filtrados);
+    }
 }
 
-function filterProducts(categoria) {
-    const filtrados = categoria === "all"
-        ? window.productos
-        : window.productos.filter(p => p.categoria === categoria);
-    mostrarProductos(filtrados);
-}
+var modelo = new Modelo();
+var vista = new Vista();
+var control = new Control(vista, modelo);
